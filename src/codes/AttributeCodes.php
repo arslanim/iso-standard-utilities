6bad4bee6255670783b7a4859a053bbf3bd866cf
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-16 23:32:55 +0400

    Added attribute codes.

---
+++ src/codes/AttributeCodes.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandardUtilities\codes;
+
+interface AttributeCodes
+{
+    public const ATTRIBUTE_ALPHA2 = 'alpha2';
+    public const ATTRIBUTE_ALPHA3 = 'alpha3';
+    public const ATTRIBUTE_NAME = 'name';
+    public const ATTRIBUTE_NUMERIC_CODE = 'numericCode';
+}
