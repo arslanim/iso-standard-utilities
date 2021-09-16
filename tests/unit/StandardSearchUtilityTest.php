06eaa5d2e68702a71ff8bd08e60549ad71de8433
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-16 23:49:52 +0400

    Created test class.

---
+++ tests/unit/StandardSearchUtilityTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandardUtilities\tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class StandardSearchUtilityTest
+ * @package arslanimamutdinov\ISOStandardUtilities\tests\unit
+ * @group unit-utility
+ */
+class StandardSearchUtilityTest extends TestCase
+{
+}
