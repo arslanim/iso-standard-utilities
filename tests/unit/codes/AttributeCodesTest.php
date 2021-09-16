be929c947ad94dd18a008e8b0de602a421e7584a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-16 23:36:14 +0400

    Added test class.

---
+++ tests/unit/codes/AttributeCodesTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandardUtilities\tests\unit\codes;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AttributeCodesTest
+ * @package arslanimamutdinov\ISOStandardUtilities\tests\unit\codes
+ * @group unit-codes
+ */
+class AttributeCodesTest extends TestCase
+{
+}
