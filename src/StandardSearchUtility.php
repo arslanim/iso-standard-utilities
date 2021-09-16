b6a8c51664b5161c9edb6e2aff2faca2c0a711e2
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-17 00:19:16 +0400

    Added utility function.

--- src/StandardSearchUtility.php
+++ src/StandardSearchUtility.php
@@ -4,8 +4,25 @@ namespace arslanimamutdinov\ISOStandardUtilities;
 
 abstract class StandardSearchUtility
 {
-    public static function existByAttributeCode(array $standardsData, string $attributeCode, string $value): bool
-    {
+    public static function getStandardsDataByAttributeCode(
+        array $standardsData,
+        string $attributeCode,
+        string $value
+    ): ?array {
+        foreach ($standardsData as $standardData) {
+            if (($standardData[$attributeCode] ?? null) === $value) {
+                return $standardData;
+            }
+        }
+
+        return null;
+    }
+
+    public static function existByAttributeCode(
+        array $standardsData,
+        string $attributeCode,
+        string $value
+    ): bool {
         return (array_search($value, array_column($standardsData, $attributeCode)) !== false) ? true : false;
     }
 }
