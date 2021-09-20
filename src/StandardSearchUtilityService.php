a267622f5b8afb7889c7067929f085362f5ac177
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-20 23:28:13 +0400

    Added existByAttributeCode.

--- src/StandardSearchUtilityService.php
+++ src/StandardSearchUtilityService.php
@@ -4,4 +4,11 @@ namespace arslanimamutdinov\ISOStandardUtilities;
 
 class StandardSearchUtilityService
 {
+    public function existByAttributeCode(
+        array $standardsData,
+        string $attributeCode,
+        string $value
+    ): bool {
+        return StandardSearchUtility::existByAttributeCode($standardsData, $attributeCode, $value);
+    }
 }
