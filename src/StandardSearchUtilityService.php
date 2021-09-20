68bb4475c69f2fe9d4fb31938294e73d7e0ffc01
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-20 23:33:57 +0400

    Added existByNumericCode.

--- src/StandardSearchUtilityService.php
+++ src/StandardSearchUtilityService.php
@@ -2,8 +2,19 @@
 
 namespace arslanimamutdinov\ISOStandardUtilities;
 
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
+
 class StandardSearchUtilityService
 {
+    public static function existByNumericCode(array $standardsData, string $numericCode): bool
+    {
+        return StandardSearchUtility::existByAttributeCode(
+            $standardsData,
+            AttributeCodes::ATTRIBUTE_NUMERIC_CODE,
+            $numericCode
+        );
+    }
+
     public function existByAttributeCode(
         array $standardsData,
         string $attributeCode,
