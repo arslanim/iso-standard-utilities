745d785420f7f7846bb4581eef7aa947b6fcbc4e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-22 14:54:52 +0400

    Covered getAllByAlpha3Values.

--- tests/unit/StandardSearchUtilityTest.php
+++ tests/unit/StandardSearchUtilityTest.php
@@ -631,6 +631,307 @@ class StandardSearchUtilityTest extends TestCase
     }
 
     /**
+     * @dataProvider getTestGetAllByAlpha3ValuesData
+     * @param array $standardsData
+     * @param string[] $values
+     * @param array $expectedResult
+     */
+    public function testGetAllByAlpha3Values(array $standardsData, array $values, array $expectedResult): void
+    {
+        $utilityResult = StandardSearchUtility::getAllByAlpha3Values($standardsData, $values);
+
+        $this->assertEquals($expectedResult, $utilityResult);
+    }
+
+    public function getTestGetAllByAlpha3ValuesData(): array
+    {
+        return [
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3',],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3', '3',],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3', '3', '3',],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3', '3', '11',],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3', '3', '11', '7'],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+                'values' => ['3', '7', '11'],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '9',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '11',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+                'values' => ['3', '10', '6'],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+                'values' => ['3',],
+                'expectedResult' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+                'values' => ['1',],
+                'expectedResult' => [],
+            ],
+            [
+                'standardsData' => [],
+                'values' => ['1',],
+                'expectedResult' => [],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+                'values' => [],
+                'expectedResult' => [],
+            ],
+        ];
+    }
+
+    /**
      * @dataProvider getTestGetAllByAttributeCodeValuesData
      * @param array $standardsData
      * @param string $attributeCode
