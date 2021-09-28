d379028d78f1e28095c4792a5e7e8b757727713f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-29 00:14:16 +0400

    Moved.

---
+++ tests/unit/functions/ExistByAlpha3Test.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandardUtilities\tests\unit\functions;
+
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
+use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtility;
+use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtilityService;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ExistByAlpha3Test
+ * @package arslanimamutdinov\ISOStandardUtilities\tests\unit\functions
+ * @group unit-utility-functions
+ */
+class ExistByAlpha3Test extends TestCase
+{
+    /**
+     * @dataProvider getTestExistByAlpha3
+     * @param array $standardsData
+     * @param string $alpha3
+     * @param bool $expectedResult
+     */
+    public function testExistByAlpha3(array $standardsData, string $alpha3, bool $expectedResult): void
+    {
+        $utilityResult = StandardSearchUtility::existByAlpha3($standardsData, $alpha3);
+        $serviceResult = (new StandardSearchUtilityService())->existByAlpha3($standardsData, $alpha3);
+
+        $this->assertEquals($expectedResult, $utilityResult);
+        $this->assertEquals($expectedResult, $serviceResult);
+        $this->assertEquals($utilityResult, $serviceResult);
+    }
+
+    public function getTestExistByAlpha3(): array
+    {
+        return [
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                ],
+                'alpha3' => '7',
+                'expectedResult' => true,
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                ],
+                'alpha3' => '3',
+                'expectedResult' => true,
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '10',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                ],
+                'alpha3' => '2',
+                'expectedResult' => false,
+            ],
+            [
+                'standardsData' => [],
+                'alpha3' => '2',
+                'expectedResult' => false,
+            ],
+            [
+                'standardsData' => [
+                    [],
+                    [],
+                    [],
+                ],
+                'alpha3' => '2',
+                'expectedResult' => false,
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '15',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
+                    ],
+                ],
+                'alpha3' => '',
+                'expectedResult' => false,
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '15',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
+                    ],
+                ],
+                'alpha3' => '900',
+                'expectedResult' => false,
+            ],
+        ];
+    }
+}
