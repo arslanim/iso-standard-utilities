13f56ac3cc2d2a7465af5f7838308a667a092ae9
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-29 22:48:38 +0400

    Moved.

---
+++ tests/unit/functions/GetAllNamesTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace arslanimamutdinov\ISOStandardUtilities\tests\unit\functions;
+
+use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
+use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtility;
+use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtilityService;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class GetAllNamesTest
+ * @package arslanimamutdinov\ISOStandardUtilities\tests\unit\functions
+ * @group unit-utility-functions
+ */
+class GetAllNamesTest extends TestCase
+{
+    /**
+     * @dataProvider getTestGetAllNumericCodes
+     * @param array $standardsData
+     * @param array $expectedResult
+     */
+    public function testGetAllNumericCodes(array $standardsData, array $expectedResult): void
+    {
+        $utilityResult = StandardSearchUtility::getAllNumericCodes($standardsData);
+        $serviceResult = (new StandardSearchUtilityService())->getAllNumericCodes($standardsData);
+
+        $this->assertEquals($expectedResult, $utilityResult);
+        $this->assertEquals($expectedResult, $serviceResult);
+        $this->assertEquals($utilityResult, $serviceResult);
+    }
+
+    public function getTestGetAllNumericCodes(): array
+    {
+        return [
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '5',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
+                    ],
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '15',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
+                    ],
+                ],
+                'expectedResult' => [
+                    '4',
+                    '8',
+                    '18',
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
+                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
+                    ],
+                ],
+                'expectedResult' => [
+                    '4',
+                ],
+            ],
+            [
+                'standardsData' => [
+                    [
+                        AttributeCodes::ATTRIBUTE_NAME => '1',
+                        AttributeCodes::ATTRIBUTE_ALPHA2 => '3',
+                        AttributeCodes::ATTRIBUTE_ALPHA3 => '4',
+                    ],
+                ],
+                'expectedResult' => [],
+            ],
+            [
+                'standardsData' => [],
+                'expectedResult' => [],
+            ],
+        ];
+    }
+}
