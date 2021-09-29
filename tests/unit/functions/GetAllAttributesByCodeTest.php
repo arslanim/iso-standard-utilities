a92fa5e1fbe3b9d6272e3ffcafc2475b01a50fac
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-29 22:34:53 +0400

    Updated.

--- tests/unit/functions/GetAllAttributesByCodeTest.php
+++ tests/unit/functions/GetAllAttributesByCodeTest.php
@@ -4,13 +4,14 @@ namespace arslanimamutdinov\ISOStandardUtilities\tests\unit\functions;
 use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
 use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtility;
 use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtilityService;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class GetAllAttributesByCodeTest
  * @package arslanimamutdinov\ISOStandardUtilities\tests\unit\functions
  * @group unit-utility-functions
  */
-class GetAllAttributesByCodeTest
+class GetAllAttributesByCodeTest extends TestCase
 {
     /**
      * @dataProvider getTestGetAllAttributesByCodeData
