e17787f396fed5a2c9bb2fd0a2c595aafc7fe67c
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2021-09-29 22:41:45 +0400

    Moved.

--- tests/unit/functions/GetAllByAlpha3ValuesTest.php
+++ tests/unit/functions/GetAllByAlpha3ValuesTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace arslanimamutdinov\ISOStandardUtilities\tests\unit;
+namespace arslanimamutdinov\ISOStandardUtilities\tests\unit\functions;
 
 use arslanimamutdinov\ISOStandardUtilities\codes\AttributeCodes;
 use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtility;
@@ -8,341 +8,28 @@ use arslanimamutdinov\ISOStandardUtilities\StandardSearchUtilityService;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Class StandardSearchUtilityTest
- * @package arslanimamutdinov\ISOStandardUtilities\tests\unit
- * @group unit-utility
+ * Class GetAllByAlpha3ValuesTest
+ * @package arslanimamutdinov\ISOStandardUtilities\tests\unit\functions
  */
-class StandardSearchUtilityTest extends TestCase
+class GetAllByAlpha3ValuesTest extends TestCase
 {
     /**
-     * @var StandardSearchUtilityService
-     */
-    private $standardSearchUtilityService;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->standardSearchUtilityService = new StandardSearchUtilityService();
-    }
-
-    /**
-     * @dataProvider getTestGetAllNamesData
-     * @param array $standardsData
-     * @param array $expectedResult
-     */
-    public function testGetAllNames(array $standardsData, array $expectedResult): void
-    {
-        $utilityResult = StandardSearchUtility::getAllNames($standardsData);
-        $serviceResult = $this->standardSearchUtilityService->getAllNames($standardsData);
-
-        $this->assertEquals($expectedResult, $utilityResult);
-        $this->assertEquals($expectedResult, $serviceResult);
-        $this->assertEquals($utilityResult, $serviceResult);
-    }
-
-    public function getTestGetAllNamesData(): array
-    {
-        return [
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '5',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '15',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
-                    ],
-                ],
-                'expectedResult' => [
-                    '1',
-                    '5',
-                    '15',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [
-                    '1',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '3',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '4',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [],
-            ],
-            [
-                'standardsData' => [],
-                'expectedResult' => [],
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getTestGetAllNumericCodes
-     * @param array $standardsData
-     * @param array $expectedResult
-     */
-    public function testGetAllNumericCodes(array $standardsData, array $expectedResult): void
-    {
-        $utilityResult = StandardSearchUtility::getAllNumericCodes($standardsData);
-        $serviceResult = $this->standardSearchUtilityService->getAllNumericCodes($standardsData);
-
-        $this->assertEquals($expectedResult, $utilityResult);
-        $this->assertEquals($expectedResult, $serviceResult);
-        $this->assertEquals($utilityResult, $serviceResult);
-    }
-
-    public function getTestGetAllNumericCodes(): array
-    {
-        return [
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '5',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '15',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
-                    ],
-                ],
-                'expectedResult' => [
-                    '4',
-                    '8',
-                    '18',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [
-                    '4',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '3',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '4',
-                    ],
-                ],
-                'expectedResult' => [],
-            ],
-            [
-                'standardsData' => [],
-                'expectedResult' => [],
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getTestGetAllAlpha3Data
-     * @param array $standardsData
-     * @param array $expectedResult
-     */
-    public function testGetAllAlpha3(array $standardsData, array $expectedResult): void
-    {
-        $utilityResult = StandardSearchUtility::getAllAlpha3($standardsData);
-        $serviceResult = $this->standardSearchUtilityService->getAllAlpha3($standardsData);
-
-        $this->assertEquals($expectedResult, $utilityResult);
-        $this->assertEquals($expectedResult, $serviceResult);
-        $this->assertEquals($utilityResult, $serviceResult);
-    }
-
-    public function getTestGetAllAlpha3Data(): array
-    {
-        return [
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '5',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '15',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
-                    ],
-                ],
-                'expectedResult' => [
-                    '3',
-                    '7',
-                    '17',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [
-                    '3',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [],
-            ],
-            [
-                'standardsData' => [],
-                'expectedResult' => [],
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getTestGetAllAlpha2Data
-     * @param array $standardsData
-     * @param array $expectedResult
-     */
-    public function testGetAllAlpha2(array $standardsData, array $expectedResult): void
-    {
-        $utilityResult = StandardSearchUtility::getAllAlpha2($standardsData);
-        $serviceResult = $this->standardSearchUtilityService->getAllAlpha2($standardsData);
-
-        $this->assertEquals($expectedResult, $utilityResult);
-        $this->assertEquals($expectedResult, $serviceResult);
-        $this->assertEquals($utilityResult, $serviceResult);
-    }
-
-    public function getTestGetAllAlpha2Data(): array
-    {
-        return [
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '5',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '6',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '7',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '8',
-                    ],
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '15',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '16',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '17',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '18',
-                    ],
-                ],
-                'expectedResult' => [
-                    '2',
-                    '6',
-                    '16',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA2 => '2',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [
-                    '2',
-                ],
-            ],
-            [
-                'standardsData' => [
-                    [
-                        AttributeCodes::ATTRIBUTE_NAME => '1',
-                        AttributeCodes::ATTRIBUTE_ALPHA3 => '3',
-                        AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
-                    ],
-                ],
-                'expectedResult' => [],
-            ],
-            [
-                'standardsData' => [],
-                'expectedResult' => [],
-            ],
-        ];
-    }
-
-    /**
-     * @dataProvider getTestGetAllByAlpha2ValuesData
+     * @dataProvider getTestGetAllByAlpha3ValuesData
      * @param array $standardsData
      * @param string[] $values
      * @param array $expectedResult
      */
-    public function testGetAllByAlpha2Values(array $standardsData, array $values, array $expectedResult): void
+    public function testGetAllByAlpha3Values(array $standardsData, array $values, array $expectedResult): void
     {
-        $utilityResult = StandardSearchUtility::getAllByAlpha2Values($standardsData, $values);
-        $serviceResult = $this->standardSearchUtilityService->getAllByAlpha2Values($standardsData, $values);
+        $utilityResult = StandardSearchUtility::getAllByAlpha3Values($standardsData, $values);
+        $serviceResult = (new StandardSearchUtilityService())->getAllByAlpha3Values($standardsData, $values);
 
         $this->assertEquals($expectedResult, $utilityResult);
         $this->assertEquals($expectedResult, $serviceResult);
-        $this->assertEquals($utilityResult, $serviceResult);
+        $this->assertEquals($utilityResult, $utilityResult);
     }
 
-    public function getTestGetAllByAlpha2ValuesData(): array
+    public function getTestGetAllByAlpha3ValuesData(): array
     {
         return [
             [
@@ -366,7 +53,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2',],
+                'values' => ['3',],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -397,7 +84,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2', '2',],
+                'values' => ['3', '3',],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -428,7 +115,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2', '2', '2',],
+                'values' => ['3', '3', '3',],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -459,7 +146,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2', '2', '10',],
+                'values' => ['3', '3', '11',],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -496,7 +183,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2', '2', '10', '6'],
+                'values' => ['3', '3', '11', '7'],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -539,7 +226,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '12',
                     ],
                 ],
-                'values' => ['2', '10', '6'],
+                'values' => ['3', '7', '11'],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -570,7 +257,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
                     ],
                 ],
-                'values' => ['2', '10', '6'],
+                'values' => ['3', '10', '6'],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
@@ -589,7 +276,7 @@ class StandardSearchUtilityTest extends TestCase
                         AttributeCodes::ATTRIBUTE_NUMERIC_CODE => '4',
                     ],
                 ],
-                'values' => ['2',],
+                'values' => ['3',],
                 'expectedResult' => [
                     [
                         AttributeCodes::ATTRIBUTE_NAME => '1',
